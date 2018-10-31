a4b0079f8e21fb30e17c8f528b44b6f4a7e2bb69
hyperrex <mrwendell@gmail.com>
2018-10-31 16:03:41 -0700

    Create route files for photos, users, projects

---
+++ src/routes/photos_routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/photos_controller');
+
+// router.get('/', ctrl.getAllPhotos);
+// router.get('/:id', ctrl.getPhotoById);
+// router.post('/', ctrl.createPhoto);
+// router.put('/:id', ctrl.updatePhoto);
+// router.delete('/:id', ctrl.deletePhoto);
+
+module.exports = router;
---
+++ src/routes/users_routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/users_controller');
+
+// router.get('/', ctrl.getAllUsers);
+// router.get('/:id', ctrl.getUserById);
+// router.post('/', ctrl.createUser);
+// router.put('/:id', ctrl.updateUser);
+// router.delete('/:id', ctrl.deleteUser);
+
+module.exports = router;
